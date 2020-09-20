8e439e3078bd7021baa7e34fefedca0354239615
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-09-19 21:07:04 -0300

    Update changecolor.js

--- changecolor.js
+++ changecolor.js
@@ -1,13 +1,13 @@
 document.addEventListener('keydown', function(colorEnter) {
 
     if(colorEnter.key == "Enter"){
-        changeColor()
+        changeColor();
     }
     })
 
     function changeColor() {
-        var body = window.document.getElementById("body")
-        var color = window.document.getElementById("color").value
-        body.style.backgroundColor = `${color}`
+        var body = window.document.getElementById("body");
+        var color = window.document.getElementById("color").value;
+        body.style.backgroundColor = `${color}`;
     
     }
